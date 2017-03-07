97a5feb3adbd370a0dc659cf76fa44f8f6ae958b
Loren C Lipsey <lipseylc@hotmail.com>
2017-03-07 10:35:40 -0500

    Created version 2.0
    Directory structure updated
    Project created
    Shiny files created

---
+++ Shiny Apps/FPDS breakdowns 2.0/server.R
@@ -0,0 +1,21 @@
+################################################################################
+# FPDS breakdowns 2.0 app - March 2017
+#
+# server.R
+################################################################################
+
+library(shiny)
+library(tidyverse)
+library(forcats)
+library(magrittr)
+library(Cairo)
+
+shinyServer(function(input, output, session) {
+
+  
+  
+  
+  
+  
+
+})
