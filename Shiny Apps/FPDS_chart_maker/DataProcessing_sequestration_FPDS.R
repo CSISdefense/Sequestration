2280fff63096d058792ae1c3411c5707c9b61644
Greg Sanders <gsanders@zo>
2017-07-18 09:44:34 -0400

    Deflators.

--- Shiny Apps/FPDS_chart_maker/DataProcessing_sequestration_FPDS.R
+++ Shiny Apps/FPDS_chart_maker/DataProcessing_sequestration_FPDS.R
@@ -45,13 +45,6 @@ FullData <- read_csv(
 
 FullData<-standardize_variable_names(Path,
                            FullData)
-
-
-
-
-
-  
-
 # coerce Amount to be a numeric variable
 FullData$Action.Obligation %<>% as.numeric()
 FullData$SumOfnumberOfActions %<>% as.numeric()
@@ -60,36 +53,18 @@ FullData$SumOfnumberOfActions %<>% as.numeric()
 FullData %<>% filter(Fiscal.Year >= 2000)
 
 
-deflate <- c(
-  "2000"= 0.707312744,
-  "2001"= 0.726215832,
-  "2002"= 0.73828541,
-  "2003"=	0.75914093,
-  "2004"=	0.779020234,
-  "2005"=	0.805910543,
-  "2006"=	0.833777068,
-  "2007"=	0.855786297,
-  "2008"=	0.885694001,
-  "2009"=	0.887468939,
-  "2010"=	0.901402201,
-  "2011"=	0.922523962,
-  "2012"=	0.940983316,
-  "2013"=	0.953319134,
-  "2014"=	0.967518637,
-  "2015"=	0.981185659,
-  "2016"=	1
-)
-
-FullData$Action.Obligation <- FullData$Action.Obligation /
-                           deflate[FullData$Fiscal.Year]
+FullData<-read_and_join(Path,
+                      "LOOKUP_Deflators.csv",
+                      FullData,
+                      by="Fiscal.Year",
+                      NA.check.columns="Deflator.2016",
+                      OnlyKeepCheckedColumns=TRUE
+)  
 
 
-
-#Apply looks and remove NAs where needed
-
-FullData<-replace_nas_with_unlabeled(FullData,"SubCustomer")
-
-FullData<-replace_nas_with_unlabeled(FullData,"PlatformPortfolio")
+FullData$Obligation.2016 <- FullData$Action.Obligation /
+  FullData$Deflator.2016
+FullData<-FullData[,colnames(FullData)!="Deflator.2016"]
 
 
 #Consolidate categories for Vendor Size
@@ -131,9 +106,10 @@ FullData<-read_and_join(Path,
 write_csv(FullData, "2016_unaggregated_FPDS.csv")
 
 
+
+PrepareLabelsAndColors(Coloration,FullData,"SubCustomer",ReplaceNAs=TRUE)
+PrepareLabelsAndColors(Coloration,FullData,"PlatformPortfolio",ReplaceNAs=TRUE)
 #Shiny.VendorSize is the new Vendor.Size
-PrepareLabelsAndColors(Coloration,FullData,"SubCustomer")
-PrepareLabelsAndColors(Coloration,FullData,"PlatformPortfolio")
 PrepareLabelsAndColors(Coloration,FullData,"Shiny.VendorSize")
 PrepareLabelsAndColors(Coloration,FullData,"Competition.sum")
 PrepareLabelsAndColors(Coloration,FullData,"Competition.multisum")
