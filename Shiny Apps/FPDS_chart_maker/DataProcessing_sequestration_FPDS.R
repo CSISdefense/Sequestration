cedeed4b1b4112b611ba323e7e9bacee95a1b800
Greg Sanders <GSanders@csis.org>
2017-08-18 14:22:59 -0400

    Implemented csis360 v. 0.0.0.9023 which has updated procedure and variable names. It also removes the need to make any k drive references.

--- Shiny Apps/FPDS_chart_maker/DataProcessing_sequestration_FPDS.R
+++ Shiny Apps/FPDS_chart_maker/DataProcessing_sequestration_FPDS.R
@@ -13,18 +13,11 @@
 # with data in the minimal form needed by Shiny script
 ################################################################################
 
+# install.packages("../csis360_0.0.0.9022.tar.gz")
+
 library(tidyverse)
 library(magrittr)
 library(csis360)
-
-install.packages("../csis360_0.0.0.9005.zip")
-Path<-"K:\\2007-01 PROFESSIONAL SERVICES\\R scripts and data\\"
-# Path<-"C:\\Users\\gsand_000.ALPHONSE\\Documents\\Development\\R-scripts-and-data\\"
-
-# source(paste(Path,"lookups.R",sep=""))
-# source(paste(Path,"helper.R",sep=""))
-
-
 
 # read in data
 FullData <- read_csv(
@@ -42,12 +35,10 @@ FullData$Fiscal.Year <- as.numeric(FullData$Fiscal.Year)
 FullData %<>% filter(Fiscal.Year >= 2000)
 
 
-FullData<-read_and_join(Path,
-                      "LOOKUP_Deflators.csv",
-                      FullData,
-                      by="Fiscal.Year",
-                      NA.check.columns="Deflator.2016",
-                      OnlyKeepCheckedColumns=TRUE
+FullData<-read_and_join(FullData,
+  "LOOKUP_Deflators.csv",
+  by="Fiscal.Year",
+  add_var="Deflator.2016"
 )
 
 
@@ -57,94 +48,90 @@ FullData<-FullData[,colnames(FullData)!="Deflator.2016"]
 
 
 #Consolidate categories for Vendor Size
-FullData<-read_and_join(Path,
-                        "LOOKUP_Contractor_Size.csv",
-                        FullData,
-                        by="Vendor.Size",
-                        NA.check.columns="Shiny.VendorSize",
-                        OnlyKeepCheckedColumns=TRUE
+FullData<-read_and_join(FullData,
+  "LOOKUP_Contractor_Size.csv",
+  by="Vendor.Size",
+  add_var="Shiny.VendorSize"
 )
 
 
 
 # classify competition
-FullData<-read_and_join(Path,
-                        "Lookup_SQL_CompetitionClassification.csv",
-                        FullData,
-                        by=c("CompetitionClassification","ClassifyNumberOfOffers"),
-                        ReplaceNAsColumns="ClassifyNumberOfOffers",
-                        NA.check.columns=c("Competition.sum",
-                                           "Competition.multisum",
-                                           "Competition.effective.only",
-                                           "No.Competition.sum"),
-                        OnlyKeepCheckedColumns=TRUE
+FullData<-read_and_join(FullData,
+  "Lookup_SQL_CompetitionClassification.csv",
+  by=c("CompetitionClassification","ClassifyNumberOfOffers"),
+  replace_na_var="ClassifyNumberOfOffers",
+  add_var=c("Competition.sum",
+    "Competition.multisum",
+    "Competition.effective.only",
+    "No.Competition.sum")
 )
 
 
 #Classify Product or Service Codes
-FullData<-read_and_join(Path,
-                        "LOOKUP_Buckets.csv",
-                        FullData,
-                        by="ProductOrServiceArea",
-                        NA.check.columns="ProductServiceOrRnDarea.sum",
-                        OnlyKeepCheckedColumns=TRUE,
-                        ReplaceNAsColumns="ProductOrServiceArea"
+FullData<-read_and_join(FullData,
+  "LOOKUP_Buckets.csv",
+  by="ProductOrServiceArea",
+  replace_na_var="ProductOrServiceArea",
+  add_var="ProductServiceOrRnDarea.sum"
 )
 
 
 FullData<-replace_nas_with_unlabeled(FullData,"SubCustomer","Uncategorized")
-FullData<-read_and_join(Path,
+FullData<-read_and_join(FullData,
                         "Lookup_SubCustomer.csv",
-                        FullData,
                         by=c("Customer","SubCustomer"),
-                        NA.check.columns="SubCustomer.platform",
-                        OnlyKeepCheckedColumns=TRUE
+                        add_var="SubCustomer.platform"
 )
 
 
-LabelsAndColors<-PrepareLabelsAndColors(FullData,"SubCustomer")
+LabelsAndColors<-prepare_labels_and_colors(FullData,"SubCustomer")
 
 FullData<-replace_nas_with_unlabeled(FullData,"PlatformPortfolio")
 LabelsAndColors<-rbind(LabelsAndColors,
-  PrepareLabelsAndColors(FullData,"PlatformPortfolio")
+  prepare_labels_and_colors(FullData,"PlatformPortfolio")
 )
 # ,"PlatformPortfolio")
 # )
 #Shiny.VendorSize is the new Vendor.Size
 LabelsAndColors<-rbind(LabelsAndColors,
-  PrepareLabelsAndColors(FullData,"Shiny.VendorSize")
+  prepare_labels_and_colors(FullData,"Shiny.VendorSize")
 )
 
 LabelsAndColors<-rbind(LabelsAndColors,
-  PrepareLabelsAndColors(FullData,"Competition.sum")
+  prepare_labels_and_colors(FullData,"Competition.sum")
 )
 
 LabelsAndColors<-rbind(LabelsAndColors,
-  PrepareLabelsAndColors(FullData,"Competition.multisum")
+  prepare_labels_and_colors(FullData,"Competition.multisum")
 )
 
 LabelsAndColors<-rbind(LabelsAndColors,
-  PrepareLabelsAndColors(FullData,"Competition.effective.only")
+  prepare_labels_and_colors(FullData,"Competition.effective.only")
 )
 
 LabelsAndColors<-rbind(LabelsAndColors,
-  PrepareLabelsAndColors(FullData,"No.Competition.sum")
+  prepare_labels_and_colors(FullData,"No.Competition.sum")
 )
 
 LabelsAndColors<-rbind(LabelsAndColors,
-  PrepareLabelsAndColors(FullData,"Customer")
+  prepare_labels_and_colors(FullData,"Customer")
 )
 
 LabelsAndColors<-rbind(LabelsAndColors,
-  PrepareLabelsAndColors(FullData,"ProductOrServiceArea")
+  prepare_labels_and_colors(FullData,"ProductOrServiceArea")
 )
 
 LabelsAndColors<-rbind(LabelsAndColors,
-  PrepareLabelsAndColors(FullData,"ProductServiceOrRnDarea.sum")
+  prepare_labels_and_colors(FullData,"ProductServiceOrRnDarea.sum")
+)
+
+LabelsAndColors<-rbind(LabelsAndColors,
+  prepare_labels_and_colors(FullData,"SubCustomer.platform")
 )
 
 # LabelsAndColors<-rbind(LabelsAndColors,
-#   PrepareLabelsAndColors(FullData,"ClassifyNumberOfOffers")
+#   prepare_labels_and_colors(FullData,"ClassifyNumberOfOffers")
 # )
 #We haven't done a key for this one. 
 
@@ -155,6 +142,7 @@ FullData %<>%
   select(-Customer) %>%
   # select(-ClassifyNumberOfOffers) %>%
   mutate(SubCustomer = factor(SubCustomer)) %>%
+  mutate(SubCustomer.platform = factor(SubCustomer.platform)) %>%
   mutate(ProductOrServiceArea = factor(ProductOrServiceArea)) %>%
   mutate(PlatformPortfolio = factor(PlatformPortfolio)) %>%
   mutate(Shiny.VendorSize = factor(Shiny.VendorSize)) %>%
