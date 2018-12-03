236973ef2d9d2ed60b3bca94a81dda7b7bcd4f0c
XinyiWang <xinyi_wang@gwmail.gwu.edu>
2018-12-03 16:41:08 -0500

    increase DPI resolution to 600

--- Shiny Apps/FPDS_chart_maker/server.R
+++ Shiny Apps/FPDS_chart_maker/server.R
@@ -415,7 +415,8 @@ shinyServer(function(input, output, session) {
         plot = mainplot(),
         width = input$save_plot_width,
         height = input$save_plot_height,
-        units = "in")
+        units = "in",
+        dpi = 600)
     }
   )
   
