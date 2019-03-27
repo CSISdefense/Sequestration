ed4d8705d16043cf7b878d4051d5a2477d3cef98
Gregory Sanders <gsanders@csis.org>
2019-03-27 16:18:10 -0400

    Typo fix.

--- Shiny Apps/FPDS_chart_maker/FPDS_breakdowns_functions.R
+++ Shiny Apps/FPDS_chart_maker/FPDS_breakdowns_functions.R
@@ -714,7 +714,7 @@ make_chart_from_input <- function(
       #When calendar and fiscal year are both in the date samplpe
       if(class(full_data[,fy_var])=="Date" & chart_geom=="Bar Chart"){
         months<-unique(month(full_data[!is.na(full_data[,y_var]),fy_var]))
-        if(all(months %in% c(4,7)) & length(month)==2){
+        if(all(months %in% c(4,7)) & length(months)==2){
           if (color_var =="None"){
             mainplot+geom_bar(aes_q(x = as.name(fy_var),y = as.name(y_var)),width=3.6, stat="identity")
           } else {
