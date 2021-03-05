c845d7be07618bdc78e2617b71d99137764ee79d
Gregory Sanders <gsanders@csis.org>
2021-03-04 23:23:43 -0500

    Update to use betFPDS data.

--- Shiny Apps/FPDS_chart_maker/DataProcessing_sequestration_FPDS.R
+++ Shiny Apps/FPDS_chart_maker/DataProcessing_sequestration_FPDS.R
@@ -21,7 +21,7 @@ library(csis360)
 library(Hmisc)
 # read in data
 full_data <- read_delim(
-  "Data//Summary.SP_CompetitionVendorSizeHistoryBucketPlatformSubCustomer.txt",delim = "\t",
+  "data//semi_clean//Summary.SP_CompetitionVendorSizeHistoryBucketPlatformSubCustomer.txt",delim = "\t",
   col_names = TRUE, col_types = "cccccccccc",na=c("NA","NULL"))
 
 
@@ -127,64 +127,112 @@ colnames(full_data)[colnames(full_data)=="Fiscal.Year"]<-"fiscal_year"
 # save(full_data,labels_and_colors,column_key, file="Shiny Apps//FPDS_chart_maker//2018_unaggregated_FPDS.Rda")
 
 # # 
-# partial_2018 <- read_delim(
-#   "Data//Single_Year_Summary_2019-02-25.csv",delim = ",")
-# colnames(partial_2018)[colnames(partial_2018)=="X9"]<-"ContractActions"
-# 
-# sum(text_to_number(partial_2018$`Action Obligation`))
-# # 
-# # 
-# partial_2018<-standardize_variable_names(partial_2018)
-# 
-# 
-# colnames(partial_2018)[colnames(partial_2018)=="Contracting.Agency.ID"]<-"AgencyID"
-# partial_2018$Action_Obligation<-text_to_number(partial_2018$Action_Obligation)
-# partial_2018$Fiscal.Year<-2018
-# sum(partial_2018$Action_Obligation)
-# 
-# 
-# partial_2018<-deflate(partial_2018,
-#                    money_var = "Action_Obligation",
-#                    deflator_var="OMB19_19"
-# )
-# 
-# partial_2018<-deflate(partial_2018,
-#                       money_var = "Action_Obligation",
-#                       deflator_var="OMB20_GDP18"
-# )
-# 
-# sum(partial_2018$Action_Obligation.Then.Year)
-# # 
-# partial_2018<-transform_contract(partial_2018)
-# 
-# 
-# 
-# partial_2018<-read_and_join(partial_2018,
-#                             path="https://raw.githubusercontent.com/CSISdefense/Lookup-Tables/master/",
-#                             "Agency_AgencyID.csv",
-#                             dir="",
-#                             by=c("AgencyID"),
-#                             add_var=c("Customer","SubCustomer","Platform"),
-#                             skip_check_var="Platform")
-# 
-# sum(partial_2018$Action_Obligation.Then.Year)
+partial <- read_delim(
+  "Data//semi_clean//Contract_Agency_ProdServ_FYear.csv",delim = ",")
 
-# partial_2018<-partial_2018%>%filter(Customer=="Defense")
+
+ota<- read_delim(
+  "Data//semi_clean//OTA_Agency_ProdServ_FYear.csv",delim = ",")
+
+# colnames(partial)[colnames(partial)=="X9"]<-"ContractActions"
 # 
-# #Classify Product or Service Codes
-# partial_2018<-csis360::read_and_join(partial_2018,
-#                                   "LOOKUP_Buckets.csv",
-#                                   # by="ProductOrServiceArea",
-#                                   by="ProductServiceOrRnDarea",
-#                                   replace_na_var="ProductServiceOrRnDarea",
-#                                   add_var="ProductServiceOrRnDarea.sum",
-#                                   path="https://raw.githubusercontent.com/CSISdefense/R-scripts-and-data/master/",
-#                                   dir="Lookups/"
-# )
+sum(text_to_number(partial$`Dollars Obligated`))
+sum(text_to_number(ota$`Dollars Obligated`))
+# # 
+# # 
+partial<-standardize_variable_names(partial)
+ota<-standardize_variable_names(ota)
 # 
 # 
-# partial_2018<-replace_nas_with_unlabeled(partial_2018,"SubCustomer","Uncategorized")
-# partial_2018<-csis360::read_and_join(partial_2018,
+colnames(partial)[colnames(partial)=="Dollars Obligated"]<-"Action_Obligation"
+colnames(partial)[colnames(partial)=="Contracting.Agency.ID"]<-"AgencyID"
+partial$Action_Obligation<-text_to_number(partial$Action_Obligation)
+
+colnames(ota)[colnames(ota)=="Dollars Obligated"]<-"Action_Obligation"
+colnames(ota)[colnames(ota)=="Contracting.Agency.ID"]<-"AgencyID"
+partial$ota<-text_to_number(ota$Action_Obligation)
+
+# 
+# 
+partial<-deflate(partial,
+                   money_var = "Action_Obligation",
+                   deflator_var="OMB19_19"
+)
+# 
+ota<-deflate(ota,
+                 money_var = "Action_Obligation",
+                 deflator_var="OMB19_19"
+)
+
+
+# 
+# # 
+# partial<-transform_contract(partial)
+# 
+# 
+# 
+partial<-read_and_join(partial,
+                            path="https://raw.githubusercontent.com/CSISdefense/Lookup-Tables/master/",
+                            "Agency_AgencyID.csv",
+                            dir="",
+                            by=c("AgencyID"),
+                            add_var=c("Customer","SubCustomer"),#,"Platform"),
+                            skip_check_var=c("Customer","SubCustomer")#Platform"
+                       )
+# 
+# sum(partial$Action_Obligation.Then.Year)
+
+partial<-partial%>%filter(Customer=="Defense")
+# 
+#Classify Product or Service Codes
+partial<-csis360::read_and_join(partial,
+                                  "ProductOrServiceCodes.csv",
+                                  by="ProductOrServiceCode",
+                                  # replace_na_var="ProductServiceOrRnDarea",
+                                  add_var=c("Simple"),
+                                path="https://raw.githubusercontent.com/CSISdefense/Lookup-Tables/master/",
+                                  dir=""
+)
+
+f<-full_data %>% group_by(fiscal_year,ProductServiceOrRnDarea.sum) %>% summarise(Action_Obligation_OMB19_19=sum(Action_Obligation_OMB19_19))
+p<-partial %>% group_by(Fiscal_Year,Simple) %>% summarise(Action_Obligation_OMB19_19=sum(Action_Obligation_OMB19_19))
+colnames(p)<-colnames(f)
+p$ProductServiceOrRnDarea.sum<-factor(p$ProductServiceOrRnDarea.sum)
+levels(p$ProductServiceOrRnDarea.sum)<-list(
+  "Products (All)"=c("Products","Products (All)"),
+  "Services (Non-R&D)"=c("Services","Services (Non-R&D)"),
+  "R&D"=c("R&D")
+)
+p<-replace_nas_with_unlabeled(p,"ProductServiceOrRnDarea.sum")
+p
+f<-rbind(f,p)
+
+f_labels_and_colors<-prepare_labels_and_colors(f,na_replaced=TRUE)
+f_column_key<-get_column_key(f)
+f$Action_Obligation_OMB19_19
+f$fiscal_year
+(topline<-build_plot(f %>% filter(ProductServiceOrRnDarea.sum!="Unlabeled"),
+               chart_geom = "Bar Chart",
+               share = FALSE,
+               labels_and_colors=f_labels_and_colors,
+               # NA, #VAR.ncol
+               x_var="fiscal_year", #x_var
+               y_var="Action_Obligation_OMB19_19", #VAR.y.variable
+               color_var="ProductServiceOrRnDarea.sum", #color_var
+               # facet_var="ContractCrisisFunding", #facet_var
+               # second_var="Is.Defense", #facet_var
+               column_key=f_column_key,
+               format=FALSE,
+               legend = TRUE
+             )      +
+    labs(y="Constant FY 2019 $ Billions")
+)
+ggsave(topline,file="Output//simple_bucket_topline.png", width=9, height=4.125)
+write.csv(f,file="output//topline.csv")           
+# 
+# 
+# partial<-replace_nas_with_unlabeled(partial,"SubCustomer","Uncategorized")
+# partial<-csis360::read_and_join(partial,
 #                                   "Lookup_SubCustomer.csv",
 #                                   by=c("Customer","SubCustomer"),
 #                                   add_var="SubCustomer.platform",
@@ -193,16 +241,16 @@ colnames(full_data)[colnames(full_data)=="Fiscal.Year"]<-"fiscal_year"
 # )
 # 
 # 
-# colnames(partial_2018)[colnames(partial_2018)=="Fiscal.Year"]<-"fiscal_year"
-# colnames(partial_2018)[colnames(partial_2018)=="ContractActions"]<-"Number.Of.Actions"
+# colnames(partial)[colnames(partial)=="Fiscal.Year"]<-"fiscal_year"
+# colnames(partial)[colnames(partial)=="ContractActions"]<-"Number.Of.Actions"
 # 
-# colnames(partial_2018)[colnames(partial_2018) %in% colnames(full_data)]
-# colnames(full_data)[!colnames(full_data) %in% colnames(partial_2018)]
-# 
-# 
+# colnames(partial)[colnames(partial) %in% colnames(full_data)]
+# colnames(full_data)[!colnames(full_data) %in% colnames(partial)]
 # 
 # 
-# partial_2018<-partial_2018 %>% group_by(ProductServiceOrRnDarea,
+# 
+# 
+# partial<-partial %>% group_by(ProductServiceOrRnDarea,
 #                                         ProductServiceOrRnDarea.sum,
 #                           SubCustomer,
 #                           SubCustomer.platform,
@@ -213,17 +261,17 @@ colnames(full_data)[colnames(full_data)=="Fiscal.Year"]<-"fiscal_year"
 # 
 # 
 # 
-# partial_2018$PlatformPortfolio<-"Unlabeled"
-# partial_2018$Vendor.Size<-"Unlabeled"
-# partial_2018$CompetitionClassification<-"Unlabeled"
-# partial_2018$ClassifyNumberOfOffers<-"Unlabeled"
-# partial_2018$Shiny.VendorSize<-"Unlabeled"
-# partial_2018$Competition.sum<-"Unlabeled"
-# partial_2018$Competition.effective.only<-"Unlabeled"
-# partial_2018$Competition.multisum<-"Unlabeled"
-# partial_2018$No.Competition.sum<-"Unlabeled"
+# partial$PlatformPortfolio<-"Unlabeled"
+# partial$Vendor.Size<-"Unlabeled"
+# partial$CompetitionClassification<-"Unlabeled"
+# partial$ClassifyNumberOfOffers<-"Unlabeled"
+# partial$Shiny.VendorSize<-"Unlabeled"
+# partial$Competition.sum<-"Unlabeled"
+# partial$Competition.effective.only<-"Unlabeled"
+# partial$Competition.multisum<-"Unlabeled"
+# partial$No.Competition.sum<-"Unlabeled"
 # 
-# full_data<-rbind(full_data,as.data.frame(partial_2018))
+# full_data<-rbind(full_data,as.data.frame(partial))
 # 
 save(full_data,labels_and_colors,column_key, file="2018_unaggregated_FPDS.Rda")
 # 
